a64ec21bee871cd37e97dc966f67011e2ee0e14b
GiacomoMaletto <malettogiacomo6@gmail.com>
2022-08-11 18:07:00 +0200

    first test

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ fileInput.onchange = async () => {
 const button = document.getElementById('button');
 button.onclick = () => {
     new Audio('notes/0.mp3').play()
-    new Audio('notes/4.mp3').play()
-    new Audio('notes/7.mp3').play()
-    new Audio('notes/12.mp3').play()
+    //new Audio('notes/4.mp3').play()
+    //new Audio('notes/7.mp3').play()
+    //new Audio('notes/12.mp3').play()
     //playSineWave(440, 3); //C
     //playSineWave(3/2 * 440, 3); //G
     //playSineWave(5/4 * 440, 3); //E
