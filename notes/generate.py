3e75e7805b964522783a49d7a954448a6166d759
GiacomoMaletto <malettogiacomo6@gmail.com>
2022-08-11 19:49:32 +0200

    playChord

--- notes/generate.py
+++ notes/generate.py
@@ -2,7 +2,7 @@ import math
 
 pitch = []
 for i in range(0, 36):
-    pitch.append( math.pow(2, i / 12.) * 440 )
+    pitch.append( math.pow(2, i / 12.) * 220 )
 
 with open("output.bat", 'w', encoding="utf8") as file:
     for i in range(0, 36):
--- script.js
+++ script.js
@@ -7,8 +7,54 @@ fileInput.onchange = async () => {
 
 const button = document.getElementById('button');
 button.onclick = () => {
-    new Audio('notes/0.mp3').play()
-    new Audio('notes/4.mp3').play()
-    new Audio('notes/7.mp3').play()
-    new Audio('notes/12.mp3').play()
+    playChord('Db7(#11)');
+}
+
+const audio = Array.from({length: 36}, (x, i) => new Audio('notes/' + i + '.mp3'));
+
+const qualityMap = new Map([
+    ['7',      [0, 4, 7, 10]],
+    ['M7',     [0, 4, 7, 11]],
+    ['m7',     [0, 3, 7, 10]],
+    ['m7(b5)', [0, 3, 6, 10]],
+    ['7(b9)',  [0, 4, 7, 13]],
+    ['7(#11)', [0, 4, 7, 18]],
+    ['6',      [0, 4, 7, 9]]
+]);
+
+const rootMap = new Map([
+    ['C',  3],
+    ['C#', 4],
+    ['Db', 4],
+    ['D',  5],
+    ['D#', 6],
+    ['Eb', 6],
+    ['E',  7],
+    ['F',  8],
+    ['F#', 9],
+    ['Gb', 9],
+    ['G',  10],
+    ['G#', 11],
+    ['Ab', 11],
+    ['A',  12],
+    ['A#', 13],
+    ['Bb', 13],
+    ['B',  14]
+]);
+
+function getRootQuality(str) {
+    for (const note of rootMap.keys()) {
+        if (str.startsWith(note)) return [note, str.slice(note.length)];
+    }
+    throw 'Undefined chord!';
+}
+
+function notationToChord(str) {
+    const [root, quality] = getRootQuality(str);
+    return qualityMap.get(quality).map((n) => n + rootMap.get(root));
+}
+
+function playChord(str) {
+    const chord = notationToChord(str);
+    for (const n of chord) audio[n].play();
 }
